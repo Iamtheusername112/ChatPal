3b5f21a5fcc689a6d185159a7d850662fe6cbf43
Iamtheusername112 <apkfem@gmail.com>
2023-10-20 14:39:14 +0200

    working on the controllers

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,10 +1,13 @@
 import express from "express";
+import morgan from "morgan";
 import { config } from "dotenv";
 const app = express();
 // Use the 'config' method to load the environment variables from the .env file
 config();
 //Middlewares
 app.use(express.json());
+app.use(morgan("dev")); //Use 'morgan' for request logging
+app.use("/api/v1");
 // Routes
 app.get("/", (req, res) => {
     res.json("Hello world");
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 import { config } from "dotenv";
 
 const app = express();
@@ -8,6 +9,9 @@ config();
 
 //Middlewares
 app.use(express.json());
+app.use(morgan("dev")); //Use 'morgan' for request logging
+
+app.use("/api/v1");
 
 // Routes
 
