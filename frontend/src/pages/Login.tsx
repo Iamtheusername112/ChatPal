bafd22a9da771c2fc79980716469a575a945966b
Iamtheusername112 <apkfem@gmail.com>
2023-11-08 23:25:27 +0100

    working on the Login, signup,and Navigation components

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,20 +1,21 @@
 import React, { useEffect } from "react";
-import { IoIosExit } from "react-icons/io";
 
-// Now you can use the IoIosLogIn icon in your component
 import { Box, Typography, Button } from "@mui/material";
 import CustomizedInput from "../components/shared/CustomizedInput.tsx";
 import { toast } from "react-hot-toast";
 import { useAuth } from "../context/AuthContext.tsx";
 import { useNavigate } from "react-router-dom";
+
 const Login = () => {
   const navigate = useNavigate();
   const auth = useAuth();
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
+
     try {
       toast.loading("Signing In", { id: "login" });
       await auth?.login(email, password);
@@ -24,24 +25,23 @@ const Login = () => {
       toast.error("Signing In Failed", { id: "login" });
     }
   };
+
   useEffect(() => {
     if (auth?.user) {
       return navigate("/chat");
     }
   }, [auth, navigate]);
+
   return (
-    <Box width={"100%"} height={"100%"} display="flex" flex={1}>
-      <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
-        <img src="airobot.png" alt="Robot" style={{ width: "400px" }} />
-      </Box>
+    <Box display="flex" flexDirection={{ xs: "column", md: "row" }}>
       <Box
-        display={"flex"}
-        flex={{ xs: 1, md: 0.5 }}
-        justifyContent={"center"}
-        alignItems={"center"}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
         padding={2}
-        ml={"auto"}
-        mt={16}
+        m="auto"
+        mt={{ xs: 2, md: 16 }}
+        width={{ xs: "100%", md: "50%" }}
       >
         <form
           onSubmit={handleSubmit}
@@ -76,7 +76,8 @@ const Login = () => {
                 px: 2,
                 py: 1,
                 mt: 2,
-                width: "400px",
+                width: "100%",
+                maxWidth: "400px", // Limit the width on larger screens
                 borderRadius: 2,
                 bgcolor: "#00fffc",
                 ":hover": {
@@ -84,12 +85,24 @@ const Login = () => {
                   color: "black",
                 },
               }}
-              endIcon={<IoIosExit />}
             >
               Login
             </Button>
           </Box>
         </form>
+      </Box>
+
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        mt={{ xs: 4, md: 0 }}
+      >
+        <img
+          src="robott.png"
+          alt="Robot"
+          style={{ width: "100%", maxWidth: "400px" }} // Adjust the maximum width
+        />
       </Box>
     </Box>
   );
